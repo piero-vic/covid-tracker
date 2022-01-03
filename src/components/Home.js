ff4a0cfed0ebeaccca23337422b43a9958bb1958
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-01-03 10:17:37 -0500

    feat(components): Add Home component
    - Create Home component
    - Add map to home page
    - Conect to redux
    - Setup router

---
+++ src/components/Home.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { BsArrowRightCircle } from 'react-icons/bs';
+import { fetchAllData } from '../redux/data/data';
+import USMap from '../us-map.png';
+
+const Home = () => {
+  const states = useSelector((state) => state.data);
+  const dispatch = useDispatch();
+
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    if (!states.length) dispatch(fetchAllData());
+  }, []);
+
+  return (
+    <div className="home-page">
+      <div className="header">all states</div>
+      <div className="main-card">
+        <img src={USMap} alt="us-map" />
+        <div>
+          <h1>United States</h1>
+        </div>
+      </div>
+      <input
+        className="search-bar"
+        type="text"
+        placeholder="SEARCH"
+        onChange={(event) => {
+          setSearchTerm(event.target.value);
+        }}
+      />
+      <div className="bar">STATS BY STATE</div>
+      <ul className="states-list">
+        {states
+          .filter((item) => item.state.toLowerCase().startsWith(searchTerm.toLowerCase()))
+          .map((item, index) => (
+            <li
+              className={`list-item ${index % 2 === 0 ? 'bg-1' : 'bg-2'}`}
+              key={item.code}
+            >
+              <Link className="state-link" to={`/${item.slug}`}>
+                <BsArrowRightCircle />
+                <img
+                  className="state-map"
+                  src={item.map_image_url}
+                  alt={`${item.slug}-map`}
+                />
+                <p className="state-name">{item.state}</p>
+                <p className="state-today-confirmed">{`Population: ${item.population}`}</p>
+              </Link>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
