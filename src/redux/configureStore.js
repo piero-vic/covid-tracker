8002d1945d75f6ab176dcddab2739d13217882c4
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-01-03 11:58:47 -0500

    feat(redux): Add reducer to and request covid data

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,9 +2,11 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import dataReducer from './data/data';
+import covidReducer from './covid/covid';
 
 const reducer = combineReducers({
   data: dataReducer,
+  covid: covidReducer,
 });
 
 const store = createStore(reducer, applyMiddleware(thunk, logger));
